9d451a017fe091e7d99d7db8e192f45416db802c
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-21 18:15:13 -0600

    completed flow control

--- if-else.go
+++ if-else.go
@@ -26,3 +26,23 @@ func PlayHand(score int) string {
     fmt.Println(action)
     return action
 }
+
+package golf
+
+
+//ultimate flow control
+func ShotsDescription(par int, shots int) string {
+  shotsOverPar := shots - par
+    if shotsOverPar == 1 {  // If the shotsOverPar variable is 1, return "bogey".
+      return "bogey"
+    } else if shotsOverPar == 0 { // If it's 0, return "par".
+      return "par"
+    } else if shotsOverPar == -1 { // For -1 return "birdie".
+      return "birdie"
+    } else if shotsOverPar == -2 { // For -2 return "eagle".
+      return "eagle"
+    } else { // For all other cases, return "no description".
+      return "no description"
+  }
+
+}
